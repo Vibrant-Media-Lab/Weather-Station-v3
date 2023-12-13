5d02431589a0f552c17959229f04096124772773
e-brent <enb40@pitt.edu>
2023-12-13 13:22:07 -0500

    Continued to update website to include daily data

--- code/weatherStationClaytonsVersion/static/js/graph_js.js
+++ code/weatherStationClaytonsVersion/static/js/graph_js.js
@@ -1,4 +1,7 @@
-// arrays for each sensor's data; global variables
+// arrays for each column of data in the CSV file - global variables
+
+// hourly data
+var timestampArray = [];
 var tempArray = [];
 var pressureArray = [];
 var humidityArray = [];
@@ -10,6 +13,18 @@ var windSpeedArray = [];
 var windHeadingArray = [];
 var rainRateArray = [];
 var fireSafetyArray = [];
+
+// daily data
+var dateArray = [];
+var highTempArray = [];
+var lowTempArray = [];
+var avgPressureArray = [];
+var highHumidityArray = [];
+var highAqiArray = [];
+var worstAqiLabelArray = [];
+var avgWindSpeedArray = [];
+var totRainFallArray = [];
+var worstFireSafetyArray = [];
 
 
 
@@ -31,97 +46,12 @@ function parseData(logFile, callBack) {
 // arguments: array of JSON objects
 // returns: nothing
 function containerFunction(data){
-    displayCurrentData(data);
     createDataArrays(data);
-
     trackButtonClicks();
 
     //checkAvailability();
 }
 
-
-// display most recent readings in the "Current Weather" section
-// arguments: array of JSON objects
-// returns: nothing
-// creates an unordered list of objects and each data type is a list item to display
-function displayCurrentData(data) {
-
-    let current_data_index = data.length - 2;   // use -2 because the array always seems to have an extra blank line at the end
-
-    //debugging for-loop to check that the data is accessible
-    /*
-    console.log(current_data_index);
-
-    for (let item in data[current_data_index]){
-        console.log(item);
-        console.log(data[current_data_index][item]);
-    }
-    */
-
-    // create ul
-    let currWeather = document.createElement("ul");
-    currWeather.id = "curr_weather";
-
-    // add currWeather to the DOM
-    let currWeatherParent = document.getElementById("live_data");
-    currWeatherParent.appendChild(currWeather);
-
-    // for each sensor measurement, create an li element, set the id and the innerText, and add it to the DOM
-    let currTemp = document.createElement("li");
-    currTemp.id = "curr_temp";
-    currTemp.innerText = `Temperature: ${data[current_data_index]["temperature"]} \u00B0C`;
-    currWeather.appendChild(currTemp);
-
-    let currPressure = document.createElement("li");
-    currPressure.id = "curr_pressure";
-    currPressure.innerText = `Air Pressure: ${data[current_data_index]["pressure"]} Pa`;
-    currWeather.appendChild(currPressure);
-
-    let currHumidity = document.createElement("li");
-    currHumidity.id = "curr_humidity";
-    currHumidity.innerText = `Humidity: ${data[current_data_index]["humidity"]} %`;
-    currWeather.appendChild(currHumidity);
-
-    let currAQILabel = document.createElement("li");
-    currAQILabel.id = "curr_AQI_label";
-    currAQILabel.innerText = `Air Quality Index (AQI) Level: ${data[current_data_index]["aqi_label"]}`;
-    currWeather.appendChild(currAQILabel);
-
-    let currAQI = document.createElement("li");
-    currAQI.id = "curr_AQI";
-    currAQI.innerText = `Air Quality Index (AQI): ${data[current_data_index]["aqi"]}`;
-    currWeather.appendChild(currAQI);
-
-    let currPM2_5 = document.createElement("li");
-    currPM2_5.id = "curr_PM_2_5";
-    currPM2_5.innerText = `PM 2.5: ${data[current_data_index]["pm25"]} \u00B5g/m3`;
-    currWeather.appendChild(currPM2_5);
-
-    let currPM10 = document.createElement("li");
-    currPM10.id = "curr_PM_10";
-    currPM10.innerText = `PM 10: ${data[current_data_index]["pm10"]} \u00B5g/m3`;
-    currWeather.appendChild(currPM10);
-
-    let currWindSpeed = document.createElement("li");
-    currWindSpeed.id = "curr_wind_speed";
-    currWindSpeed.innerText = `Wind Speed: ${data[current_data_index]["wind_speed"]} mph`;
-    currWeather.appendChild(currWindSpeed);
-
-    let currWindHeading = document.createElement("li");
-    currWindHeading.id = "curr_wind_heading";
-    currWindHeading.innerText = `Wind Heading: ${data[current_data_index]["wind_heading"]} \u00B0`;
-    currWeather.appendChild(currWindHeading);
-
-    let currRainRate = document.createElement("li");
-    currRainRate.id = "curr_rain_rate";
-    currRainRate.innerText = `Rain Rate: ${data[current_data_index]["rain_rate"]} inches per hour`;
-    currWeather.appendChild(currRainRate);
-
-    let currFireSafety = document.createElement("li");
-    currFireSafety.id = "curr_fire_safety";
-    currFireSafety.innerText = `Fire Safety Rating: ${data[current_data_index]["fire_safety_rating"]}`;
-    currWeather.appendChild(currFireSafety);
-}
 
 // create arrays with each individual data type; store to the global variables so they are accessible for making graphs
 // arguments: array of JSON objects
@@ -129,6 +59,7 @@ function displayCurrentData(data) {
 function createDataArrays(data){
 
     // reset the arrays to be empty to make sure that data is not just being added on to the end every time this is called
+    timestampArray = [];
     tempArray = [];
     pressureArray = [];
     humidityArray = [];
@@ -140,13 +71,27 @@ function createDataArrays(data){
     windHeadingArray = [];
     rainRateArray = [];
     fireSafetyArray = [];
+
+    dateArray = [];
+    highTempArray = [];
+    lowTempArray = [];
+    avgPressureArray = [];
+    highHumidityArray = [];
+    highAqiArray = [];
+    worstAqiLabelArray = [];
+    avgWindSpeedArray = [];
+    totRainFallArray = [];
+    worstFireSafetyArray = [];
     
 
 
     let current_data_length = data.length - 1;  // subtract 1 to get length because of empty line at the end
 
     // for each object in the array, add the reading for that data type to the end of the appropriate array
+
+    // hourly data
     for (let i = 0; i < current_data_length; i++){
+        timestampArray.push(data[i]["timestamp"]);
         tempArray.push(data[i]["temperature"]);
         pressureArray.push(data[i]["pressure"]);
         humidityArray.push(data[i]["humidity"]);
@@ -160,12 +105,27 @@ function createDataArrays(data){
         fireSafetyArray.push(data[i]["fire_safety_rating"]);
         
     }
+
+    // daily data
+    for (let i = 0; i < current_data_length; i+=24){
+        dateArray.push(data[i]["prev_date"]);
+        highTempArray.push(data[i]["prev_day_high_temp"]);
+        lowTempArray.push(data[i]["prev_day_low_temp"]);
+        avgPressureArray.push(data[i]["prev_day_avg_pressure"]);
+        highHumidityArray.push(data[i]["prev_day_high_humidity"]);
+        highAqiArray.push(data[i]["prev_day_high_aqi"]);
+        worstAqiLabelArray.push(data[i]["prev_day_worst_aqi_label"]);
+        avgWindSpeedArray.push(data[i]["prev_day_avg_wind_speed"]);
+        totRainFallArray.push(data[i]["prev_day_total_rain"]);
+        worstFireSafetyArray.push(data[i]["prev_day_worst_fine_safety"]);
+    }
 }
 
 
 // debugging function to check scope of arrays; make sure that the global variables are working as intended
 /*
 function checkAvailability(){
+    console.log(timestampArray);
     console.log(tempArray);
     console.log(pressureArray);
     console.log(humidityArray);
@@ -177,6 +137,19 @@ function checkAvailability(){
     console.log(windHeadingArray);
     console.log(rainRateArray);
     console.log(fireSafetyArray);
+
+    console.log(dateArray);
+    console.log(highTempArray);
+    console.log(lowTempArray);
+    console.log(avgPressureArray);
+    console.log(highHumidityArray);
+    console.log(highAqiArray);
+    console.log(worstAqiLabelArray);
+    console.log(avgWindSpeedArray);
+    console.log(totRainFallArray);
+    console.log(worstFireSafetyArray);
+    
+
 }
 */
 
